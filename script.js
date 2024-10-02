0b2d0a823ff9748252cb236119eca77f189df99d
Pardo2273 <mpita38@gmail.com>
2024-10-01 23:55:50 -0600

    el siguiente commit posee las primeras dos funciones del registro escolar las cuales muestra las notas del estudiante y el promedio

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-//brindar las 4 funciones de los botones
+//captamos el elemento ul
+let elementoLista = document.getElementById('lista');
+
+//esto para dar los otros resultados de las funciones
+let respuesta= document.getElementById('resultado');
+
+//arreglo que se emplea para las 5 notas
+const arreglo = [100, 40, 50, 65, 70];
+
+function mostrarNotas(){
+    //captamos la lista para agregarle las notas
+    for(nota of arreglo){
+        //armamos el item de la lista
+        let texto= "Nota: " + nota;
+
+        let itemLista = document.createElement("li");
+        itemLista.innerText = texto;
+
+        elementoLista.appendChild(itemLista);
+    }
+}
+
+function promedio(){
+    let promedio = 0;
+    for(let x = 0; x < arreglo.length; x++){
+        promedio = promedio + arreglo[x];
+    }
+    promedio = promedio/arreglo.length;
+    
+    respuesta.textContent = "El promedio es de: " + promedio;
+}
+
+//por agregar
+function mayor(){
+
+}
+
+function aplazado(){
+
+}
