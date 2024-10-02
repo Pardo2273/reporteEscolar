99cb3ef59c5ee4a2e00dc58e795654fa9ef76414
Pardo2273 <mpita38@gmail.com>
2024-10-02 13:05:36 -0600

    se adjuntan muchos cambios tanto en diseno, las funciones que faltaban se adjuntan que son las de ver la nota mayor y si hay o no aplazados, asimismo cambiaron ciertas cosas del html

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 let elementoLista = document.getElementById('lista');
 
 //esto para dar los otros resultados de las funciones
-let respuesta= document.getElementById('resultado');
+let prom = document.getElementById('promedio');
+let notaAlta = document.getElementById('notaAlta');
+let aprobado = document.getElementById('aplazado');
 
 //arreglo que se emplea para las 5 notas
-const arreglo = [100, 40, 50, 65, 70];
+const arreglo = [10, 40, 50, 65, 70];
 
 function mostrarNotas(){
     //captamos la lista para agregarle las notas
@@ -23,18 +25,43 @@ function mostrarNotas(){
 function promedio(){
     let promedio = 0;
     for(let x = 0; x < arreglo.length; x++){
+        //se realiza la suma
         promedio = promedio + arreglo[x];
     }
+    //se hace la division para  el promedio
     promedio = promedio/arreglo.length;
     
-    respuesta.textContent = "El promedio es de: " + promedio;
+    //se brinda el resultado cambiando el texto del elemento respuesta
+    prom.textContent = promedio;
 }
 
 //por agregar
 function mayor(){
-
+    //dos variables que son vitales para lograr encontrar el mayor en el arreglo
+    let contador= 0;
+    let notaMayor = 0;
+    //mientras el contador sea distinto a la cantidad de valores que posee el arreglo este se seguira ejecutando
+    while(contador != arreglo.length){
+        if(notaMayor > arreglo[contador]){
+            notaMayor = notaMayor;
+        }else{
+            notaMayor = arreglo[contador];
+        }
+        contador++;
+    }
+    //finalmente devolvemos el valor mayor de las notas que estan dentro del array
+    notaAlta.textContent = notaMayor;
 }
 
+//como solo intere saber si hay o no aplazados por eso se coloca de la siguiente manera
 function aplazado(){
-
+    let contador = 0;
+    do{
+        //si una nota es menor a 65 se brinda la respuesta de que existe aplazados y de una vez se para el sistema
+        if(arreglo[contador] < 65){
+            aprobado.textContent = "Si hay aplazados";
+            break;
+        }
+        contador++;
+    }while(contador != arreglo.length);// mientras 
 }
